b305f17980fe3f36c22cf952ba22f4214acc9a56
SimonStockinger <simo.stockinger@gmail.com>
2026-02-06 00:00:29 +0100

    Updated Install Script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-sudo ln -sf "$(pwd)/cheatsheets" /usr/local/bin/cheatsheets
+sudo ln -sf "$(pwd)/cheatsheets.sh" /usr/local/bin/cheatsheets
 echo "Installed globally! Run 'cheatsheets' from anywhere."
 
